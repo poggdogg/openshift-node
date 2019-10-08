8a084c2e6bcf71fb559f35642cd154ce175db6b8
Martin <183794@NTTDATA.COM>
2019-10-08 13:02:04 -0700

    added more wild to whitelist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   version: '1.0.0',
   apiEndpoint: '/api/v1',
-  whiteListedDomain: ['localhost:5000', 'openshift-node-dqszvc-dev.pathfinder.gov.bc.ca','http://openshift-node-dqszvc-dev.pathfinder.gov.bc.ca']
+  whiteListedDomain: ['localhost:5000', 'openshift-node-dqszvc-dev.pathfinder.gov.bc.ca','http://openshift-node-dqszvc-dev.pathfinder.gov.bc.ca','*.pathfinder.gov.bc.ca','.*.pathfinder.gov.bc.ca']
 };
